109e25c5600ddb5ffc61b3f4447acbc1b0a89545
Halima <natashawanjiku31@gmail.com>
2025-06-01 12:55:32 -0400

    Add more 5 solutions to milestone 2

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -52,3 +52,65 @@ function countProperties(properties) {
     console.log(keys.length);
 }
 countProperties({ name: "Alice", age: 25, city: "Paris" });
+//6. Filter by Length
+function filterByLength(words, minLength) {
+    var result = words.filter(function (words) { return words.length >= minLength; });
+    console.log(result);
+}
+filterByLength(["cat", "giraffe", "hippo", "dog", "elephant"], 5);
+//7. Sum of Even Numbers
+function sumEvenNumbers(array) {
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            sum = sum + array[i];
+        }
+    }
+    console.log(sum);
+}
+sumEvenNumbers([1, 2, 3, 4, 5, 6]);
+//8. Difference Between Sum of Even and Odd Numbers
+function differenceEvenOdd(array) {
+    var sumOdd = 0;
+    var sumEven = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            sumEven = sumEven + array[i];
+        }
+        else {
+            sumOdd = sumOdd + array[i];
+        }
+    }
+    var difference = sumEven - sumOdd;
+    console.log(difference);
+}
+differenceEvenOdd([1, 2, 3, 4, 5, 6]);
+function countTruthy(myObject) {
+    var count = 0;
+    var properties = Object.keys(myObject);
+    for (var i = 0; i < properties.length; i++) {
+        var key = properties[i];
+        var value = myObject[key];
+        if (value) {
+            count++;
+        }
+    }
+    console.log(count);
+}
+console.log(countTruthy({ a: 0, b: "hello", c: false, d: 42, e: null }));
+//10. Average of Numbers
+function average(array) {
+    var average = 0;
+    if (array.length === 0) {
+        console.log(0);
+        return;
+    }
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+        average = sum / (array.length);
+    }
+    console.log(average);
+}
+average([2, 4, 6, 8]);
+average([]);
--- milestone_2_challenges.ts
+++ milestone_2_challenges.ts
@@ -75,3 +75,81 @@ function sumOfPositives(num:number[]):void{
     console.log(keys.length)
    }
    countProperties({ name: "Alice", age: 25, city: "Paris" })
+
+//6. Filter by Length
+
+function filterByLength(words:string[],minLength:number):void{
+    let result =words.filter((words)=>words.length>=minLength)
+    console.log(result)
+}
+filterByLength(["cat", "giraffe", "hippo", "dog", "elephant"], 5)
+
+//7. Sum of Even Numbers
+function sumEvenNumbers(array:number[]):void{
+    let sum = 0;
+
+    for(let i = 0;i<array.length;i++){
+        if(array[i]%2===0){
+            sum=sum+array[i]
+        }
+    }
+    console.log(sum)
+}
+sumEvenNumbers([1, 2, 3, 4, 5, 6])
+
+//8. Difference Between Sum of Even and Odd Numbers
+function differenceEvenOdd(array:number[]):void{
+    let sumOdd =0
+    let sumEven =0
+
+    for(let i= 0;i<array.length;i++){
+        if(array[i]%2===0){
+            sumEven = sumEven + array[i]
+        }else {
+            sumOdd = sumOdd +array[i]
+        }
+    }
+    let difference = sumEven - sumOdd
+    console.log(difference)
+}
+differenceEvenOdd([1, 2, 3, 4, 5, 6])
+
+interface MyObject {
+    [key: string]: number | null | undefined | boolean | string;
+  }
+  
+  function countTruthy(myObject: MyObject): void {
+    let count = 0;
+    const properties = Object.keys(myObject);
+  
+    for (let i = 0; i < properties.length; i++) {
+      const key = properties[i];
+      const value = myObject[key];
+  
+      if (value) {
+        count++;
+      }
+    }
+  
+    console.log(count);
+  }
+  
+  console.log(countTruthy({ a: 0, b: "hello", c: false, d: 42, e: null }));
+  
+  //10. Average of Numbers
+
+  function average(array:number[]):void{
+    let average= 0
+    if(array.length===0){
+        console.log(0)
+        return;
+    }
+    let sum = 0
+    for(let i =0;i<array.length;i++){
+        sum = sum + array[i]
+        average = sum / (array.length)
+    }
+    console.log(average)
+  }
+  average([2, 4, 6, 8])
+  average([])
